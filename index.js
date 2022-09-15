1e14115fb81e907b9d141070b2c02b5c31278cfe
Bruno Silveira <bruno.silveira02@hotmail.com>
2022-09-15 14:12:53 +0200

    add delete button

---
+++ index.js
@@ -0,0 +1,38 @@
+//save the input into an array or something ( your model ) 
+//const myTodoItems = ["wash the car", "learn JavaScript", "don't do any CSS"]
+const todoItems = []
+
+//select the input tag from your javascript (querySelector)
+const input = document.querySelector("input")
+const list = document.querySelector("#listItems")
+
+//delete item
+function deleteItem(i) {
+    todoItems.splice(i, 1)
+    render() 
+} 
+
+//listen to the input tag variable ( add an eventListener ) 
+//for when the user hits the "Enter" key hint "keyup" event.key === "Enter"
+input.addEventListener("keyup", function(send) {
+    if (send.key === "Enter") {
+        const newInput = {
+            dene: false,
+            text: input.value
+        }
+        todoItems.unshift(newInput)
+        input.value = ""
+        render()
+    }
+})
+
+
+function render() {
+    const newRender = todoItems
+    .map((item, i) => `<li>${item.text}</li>
+    <button onclick="deleteItem(${i})">🗑</button>`)
+    .join("")
+    list.innerHTML = newRender
+}
+
+render()
--- script.js
+++ script.js
@@ -5,19 +5,28 @@ const todoItems = []
 //select the input tag from your javascript (querySelector)
 const input = document.querySelector("input")
 const list = document.querySelector("#listItems")
+const send_button = document.querySelector("#send")
+
+//delete item
+function deleteItem() {
+    todoItems.splice(0, 1)  
+} 
 
 //listen to the input tag variable ( add an eventListener ) 
 //for when the user hits the "Enter" key hint "keyup" event.key === "Enter"
-function keyUp(event) {
+function render(event) {
     if (event.key === "Enter") {
+    todoItems.unshift(event.target.value)
 //update the HTML on the page to represent the new state of your model 
 //( to show the new item in the array on screen).
     listItems.innerHTML += `
-    <li>${event.target.value}</li>
+    <li>${event.target.value}
+    <button onclick="deleteItem()">🗑</button>
+    </li>
     `
 //empty the HTML input tag so the user can type a new thing in    
     input.value = ""
     }
 }
 
-input.addEventListener("keyup", keyUp)
+input.addEventListener("keyup", render)
